0fd251f914b8887fbc8d419598dbad56e3c4043a
has0825 <k024g0026@g.neec.ac.jp>
2025-09-29 12:27:03 +0900

    00-01と01-01が入ってなかったことを解消

---
+++ 0-01/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <windows.h>
+
+int main() {
+
+    std::printf("こんにちは\n");
+
+    return 0;
+}
---
+++ 01-01/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <string>
+using namespace std;
+
+int main() {
+    list<const char*> stations = {
+        "Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno",
+        "Uguisudani", "Nippori", "Tabata", "Komagome", "Sugamo",
+        "Otsuka", "Ikebukuro", "Mejiro", "Takadanobaba", "Shin-Okubo",
+        "Shinjuku", "Yoyogi", "Harajuku", "Shibuya", "Ebisu",
+        "Meguro", "Gotanda", "Osaki", "Shinagawa", "Tamachi",
+        "Hamamatsucho", "Shimbashi", "Yurakucho"
+    };
+
+    cout << "Yamanote Line (1970):" << endl;
+    for (auto s : stations) {
+        cout << s << " ";
+    }
+    cout << endl << endl;
+
+    {
+        auto it = stations.begin();
+        while (it != stations.end()) {
+            if (string(*it) == "Tabata") {
+                stations.insert(it, "Nishi-Nippori");
+                break;
+            }
+            ++it;
+        }
+    }
+
+    cout << "Yamanote Line (2019):" << endl;
+    for (auto s : stations) {
+        cout << s << " ";
+    }
+    cout << endl << endl;
+
+    {
+        auto it = stations.begin();
+        while (it != stations.end()) {
+            if (string(*it) == "Tamachi") {
+                stations.insert(it, "Takanawa Gateway");
+                break;
+            }
+            ++it;
+        }
+    }
+
+    cout << "Yamanote Line (2022):" << endl;
+    for (auto s : stations) {
+        cout << s << " ";
+    }
+    cout << endl << endl;
+
+    return 0;
+}
