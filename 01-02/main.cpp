f7ce6930fd0da46f9c8e8c43a34b699dba1e4d6d
has0825 <k024g0026@g.neec.ac.jp>
2025-09-29 12:24:35 +0900

    完了

---
+++ 01-02/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+bool compareStudentId(const std::string& a, const std::string& b) {
+    auto extractId = [](const std::string& mail) {
+        return mail.substr(0, mail.find('@'));
+        };
+
+    std::string idA = extractId(a);
+    std::string idB = extractId(b);
+
+    int yearA, numA;
+    int yearB, numB;
+    char k, g;
+    std::stringstream(idA) >> k >> yearA >> g >> numA;
+    std::stringstream(idB) >> k >> yearB >> g >> numB;
+
+    if (yearA != yearB) return yearA < yearB;
+    return numA < numB;
+}
+
+int main() {
+    std::ifstream file("PG3_2025_01_02.txt");
+    if (!file) {
+        std::cerr << "ファイルを開けませんでした\n";
+        return 1;
+    }
+
+    std::string line;
+    std::getline(file, line);
+    file.close();
+
+    std::vector<std::string> emails;
+    std::stringstream ss(line);
+    std::string token;
+    while (std::getline(ss, token, ',')) {
+        if (!token.empty()) {
+            emails.push_back(token);
+        }
+    }
+
+    std::sort(emails.begin(), emails.end(), compareStudentId);
+
+    for (const auto& e : emails) {
+        std::cout << e << std::endl;
+    }
+
+    return 0;
+}
