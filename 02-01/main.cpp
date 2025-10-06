81f38e0eebe5f8bc84c672e5318245cf7fef69fa
has0825 <k024g0026@g.neec.ac.jp>
2025-10-06 11:10:50 +0900

    02-01完了

---
+++ 02-01/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// --- 関数テンプレート Min() ---
+template <typename T>
+T Min(T a, T b) {
+    return (a < b) ? a : b;
+}
+
+int main() {
+    // int型
+    int a = 10, b = 20;
+    cout << "Min(int): " << Min(a, b) << endl;
+
+    // float型
+    float x = 3.14f, y = 1.59f;
+    cout << "Min(float): " << Min(x, y) << endl;
+
+    // double型
+    double p = 2.718, q = 3.14159;
+    cout << "Min(double): " << Min(p, q) << endl;
+
+    return 0;
+}
