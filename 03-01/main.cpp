bf420523c957fe9b41346e887df518d8d06ffb5c
has0825 <k024g0026@g.neec.ac.jp>
2025-11-26 10:33:00 +0900

    03-01完了

---
+++ 03-01/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+
+enum class Phase {
+    Approach,   // 接近
+    Shoot,      // 射撃
+    Retreat,    // 離脱
+
+    Max,        // 状態数（終了判定用）
+};
+
+class Enemy {
+public:
+    Enemy();
+    bool Update();
+
+private:
+   
+    using StateFunc = void (Enemy::*)();
+
+    static StateFunc stateTable[];
+
+    Phase phase_;
+
+    void Approach();
+    void Shoot();
+    void Retreat();
+};
+
+
+Enemy::StateFunc Enemy::stateTable[] = {
+    &Enemy::Approach, // [0] 接近
+    &Enemy::Shoot,    // [1] 射撃
+    &Enemy::Retreat   // [2] 離脱
+};
+
+Enemy::Enemy() : phase_(Phase::Approach) {
+
+}
+
+void Enemy::Approach() {
+    std::cout << "敵の状態:接近" << std::endl;
+
+    phase_ = Phase::Shoot;
+}
+
+void Enemy::Shoot() {
+    std::cout << "敵の状態:射撃" << std::endl;
+
+    phase_ = Phase::Retreat;
+}
+
+void Enemy::Retreat() {
+    std::cout << "敵の状態:離脱" << std::endl;
+
+    phase_ = Phase::Max;
+}
+
+
+bool Enemy::Update() {
+
+    if (phase_ >= Phase::Max) {
+        return false;
+    }
+
+    size_t index = static_cast<size_t>(phase_);
+
+    (this->*stateTable[index])();
+
+    return true;
+}
+
+int main() {
+    Enemy enemy;
+
+    std::cout << "--- シミュレーション開始 ---" << std::endl;
+
+    while (true) {
+
+        if (!enemy.Update()) {
+            break;
+        }
+    }
+
+    std::cout << "--- シミュレーション終了 ---" << std::endl;
+
+    return 0;
+}
