79eb5924b9667958abc8e7d98a1c8e8b2c08500e
has0825 <k024g0026@g.neec.ac.jp>
2025-11-26 11:21:09 +0900

    03-03完了

---
+++ 03-03/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+class Animal {
+protected:
+    std::string species; 
+
+public:
+
+    Animal(std::string s) : species(s) {}
+
+    virtual ~Animal() {}
+
+    virtual void Cry() {
+        std::cout << "（正体不明の動物が鳴いています...）" << std::endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    Dog() : Animal("犬") {}
+
+    void Cry() override {
+        std::cout << "[" << species << "] が吠えました: ワンワン" << std::endl;
+    }
+};
+
+class Cat : public Animal {
+public:
+    Cat() : Animal("猫") {}
+
+    void Cry() override {
+        std::cout << "[" << species << "] が鳴きました:ニャー" << std::endl;
+    }
+};
+
+class Sheep : public Animal {
+public:
+    Sheep() : Animal("羊") {}
+
+    void Cry() override {
+        std::cout << "[" << species << "] が鳴きました:メェ～" << std::endl;
+    }
+};
+
+int main() {
+    Animal* zoo[3];
+
+    zoo[0] = new Dog();
+    zoo[1] = new Cat();
+    zoo[2] = new Sheep();
+
+
+    for (int i = 0; i < 3; i++) {
+        zoo[i]->Cry();
+    }
+
+
+    for (int i = 0; i < 3; i++) {
+        delete zoo[i];
+    }
+
+    return 0;
+}
