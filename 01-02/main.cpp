91f7dbb3d9d5f135d77de0e6c726152cf66cdc72
has0825 <k024g0026@g.neec.ac.jp>
2026-01-19 10:31:42 +0900

    01_02を修正

--- 01-02/main.cpp
+++ 01-02/main.cpp
@@ -6,44 +6,54 @@
 #include <algorithm>
 
 bool compareStudentId(const std::string& a, const std::string& b) {
-	auto extractId = [](const std::string& mail) {
-		return mail.substr(0, mail.find('@'));
-		};
 
-	std::string idA = extractId(a);
-	std::string idB = extractId(b);
+    auto extractId = [](const std::string& mail) {
 
-	int yearA, numA;
-	int yearB, numB;
-	char k, g;
-	std::stringstream(idA) >> k >> yearA >> g >> numA;
-	std::stringstream(idB) >> k >> yearB >> g >> numB;
+        return mail.substr(0, mail.find('@'));
+        };
 
-	if (yearA != yearB) return yearA < yearB;
-	return numA < numB;
+    std::string idA = extractId(a);
+    std::string idB = extractId(b);
+
+    int yearA, numA;
+    int yearB, numB;
+    char k, g;
+
+
+    std::stringstream(idA) >> k >> yearA >> g >> numA;
+    std::stringstream(idB) >> k >> yearB >> g >> numB;
+
+    if (yearA != yearB) return yearA < yearB;
+    return numA < numB;
 }
 
 int main() {
-	std::ifstream file("PG3_2025_01_02.txt");
+    std::ifstream file("PG3_2025_01_02.txt");
 
-	std::string line;
-	std::getline(file, line);
-	file.close();
+    if (!file.is_open()) {
+        std::cerr << "ファイルを開けませんでした。" << std::endl;
+        return 1;
+    }
 
-	std::vector<std::string> emails;
-	std::stringstream ss(line);
-	std::string token;
-	while (std::getline(ss, token, ',')) {
-		if (!token.empty()) {
-			emails.push_back(token);
-		}
-	}
+    std::string line;
+    std::getline(file, line);
+    file.close();
 
-	std::sort(emails.begin(), emails.end(), compareStudentId);
+    std::vector<std::string> emails;
+    std::stringstream ss(line);
+    std::string token;
 
-	for (const auto& e : emails) {
-		std::cout << e << std::endl;
-	}
+    while (std::getline(ss, token, ',')) {
+        if (!token.empty()) {
+            emails.push_back(token);
+        }
+    }
 
-	return 0;
+    std::sort(emails.begin(), emails.end(), compareStudentId);
+
+    for (const auto& e : emails) {
+        std::cout << e << std::endl;
+    }
+
+    return 0;
 }
